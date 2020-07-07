1b4ca34da8ad1cb0ab3b541eb82d521d1c76c0ec
j2reigh <58107794+j2reigh@users.noreply.github.com>
2020-07-07 21:57:03 +0900

    [Fix]

--- STORM/STORM/CollecViewController2.swift
+++ STORM/STORM/CollecViewController2.swift
@@ -35,7 +35,7 @@ class CollecViewController2: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return 12
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -52,17 +52,12 @@ class CollecViewController2: UICollectionViewController {
             cell.backgroundColor = UIColor.black
         case 2:
             cell.backgroundColor = UIColor.blue
-
         default:
             break
-
         }
 
         return cell
     }
-
-
-    
 }
 
 public enum UPCarouselFlowLayoutSpacingMode {
@@ -80,19 +75,22 @@ public class UPCarouselFlowLayout: UICollectionViewFlowLayout {
         }
     }
 
-    @IBInspectable public var sideItemScale: CGFloat = 0.6
-    @IBInspectable public var sideItemAlpha: CGFloat = 0.6
-    public var spacingMode = UPCarouselFlowLayoutSpacingMode.fixed(spacing: 40)
+    @IBInspectable public var sideItemScale: CGFloat = 0.757//0.757
+    @IBInspectable public var sideItemAlpha: CGFloat = 0.5
+    public var spacingMode = UPCarouselFlowLayoutSpacingMode.fixed(spacing: 5)
 
     private var state = LayoutState(size: .zero, direction: .horizontal)
 
-
+    // Tells the layout object to update the current layout.
     override public func prepare() {
         super.prepare()
-
+        
+        print("prepare 실행")
+        print(self.collectionView!.bounds.size, self.scrollDirection)
         let currentState = LayoutState(size: self.collectionView!.bounds.size, direction: self.scrollDirection)
 
         if !self.state.isEqual(otherState: currentState) {
+            print("실행2")
             self.setupCollectionView()
             self.updateLayout()
             self.state = currentState
@@ -100,30 +98,30 @@ public class UPCarouselFlowLayout: UICollectionViewFlowLayout {
     }
 
     private func setupCollectionView() {
-        guard let collectionView = self.collectionView else { return }
-        if collectionView.decelerationRate != UIScrollView.DecelerationRate.fast {
-            collectionView.decelerationRate = UIScrollView.DecelerationRate.fast
-        }
+//        guard let collectionView = self.collectionView else { return }
+//        if collectionView.decelerationRate != UIScrollView.DecelerationRate.fast {
+//            collectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+//        }
     }
 
     private func updateLayout() {
         guard let collectionView = self.collectionView else { return }
 
         let collectionSize = collectionView.bounds.size
-        let isHorizontal = (self.scrollDirection == .horizontal)
+//        let isHorizontal = (self.scrollDirection == .horizontal)
 
         let yInset = (collectionSize.height - self.itemSize.height) / 2
         let xInset = (collectionSize.width - self.itemSize.width) / 2
         self.sectionInset = UIEdgeInsets(top: yInset, left: xInset, bottom: yInset, right: xInset)
 
-        let side = isHorizontal ? self.itemSize.width : self.itemSize.height
+        let side = self.itemSize.width
         let scaledItemOffset =  (side - side*self.sideItemScale) / 2
         switch self.spacingMode {
         case .fixed(let spacing):
             self.minimumLineSpacing = spacing - scaledItemOffset
         case .overlap(let visibleOffset):
             let fullSizeSideItemOverlap = visibleOffset + scaledItemOffset
-            let inset = isHorizontal ? xInset : yInset
+            let inset = xInset
             self.minimumLineSpacing = inset - fullSizeSideItemOverlap
         }
     }
@@ -132,6 +130,7 @@ public class UPCarouselFlowLayout: UICollectionViewFlowLayout {
         return true
     }
     
+    // Returns the layout attributes for all of the cells and views in the specified rectangle.
     public override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         guard let superAttributes = super.layoutAttributesForElements(in: rect),
             let attributes = NSArray(array: superAttributes, copyItems: true) as? [UICollectionViewLayoutAttributes]
@@ -141,21 +140,32 @@ public class UPCarouselFlowLayout: UICollectionViewFlowLayout {
 
     private func transformLayoutAttributes(attributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
         guard let collectionView = self.collectionView else { return attributes }
-        let isHorizontal = (self.scrollDirection == .horizontal)
+//        let isHorizontal = (self.scrollDirection == .horizontal)
 
-        let collectionCenter = isHorizontal ? collectionView.frame.size.width/2 : collectionView.frame.size.height/2
-        let offset = isHorizontal ? collectionView.contentOffset.x : collectionView.contentOffset.y
-        let normalizedCenter = (isHorizontal ? attributes.center.x : attributes.center.y) - offset
+        let collectionCenter = collectionView.frame.size.width/2
+        let offset = collectionView.contentOffset.x
+        let normalizedCenter = attributes.center.x - offset
 
-        let maxDistance = (isHorizontal ? self.itemSize.width : self.itemSize.height) + self.minimumLineSpacing
+        let maxDistance = self.itemSize.width + self.minimumLineSpacing
         let distance = min(abs(collectionCenter - normalizedCenter), maxDistance)
         let ratio = (maxDistance - distance)/maxDistance
-
+        
+        var test = Float()
+        
+//        if normalizedCenter < distance * 3 + 5 {
+//            test = normalizedCenter
+//        }
+        
         let alpha = ratio * (1 - self.sideItemAlpha) + self.sideItemAlpha
         let scale = ratio * (1 - self.sideItemScale) + self.sideItemScale
+        
         attributes.alpha = alpha
         attributes.transform3D = CATransform3DScale(CATransform3DIdentity, scale, scale, 1)
-
+        
+//        print(maxDistance, distance)
+        print(offset, normalizedCenter, distance, abs(collectionCenter - normalizedCenter), ratio, scale  )
+        print("scale \(ratio) \(scale) \(self.sideItemScale)")
+        
         return attributes
     }
     
