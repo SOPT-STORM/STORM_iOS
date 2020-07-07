7a7576e910c5955c33ec155cb859e5718ea4e330
j2reigh <58107794+j2reigh@users.noreply.github.com>
2020-07-07 20:34:49 +0900

    Add Project Wating ViewController

---
+++ STORM/STORM/Jiyoon/Wrapup/Controller/ProjectWaitingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectWaitingViewController.swift
+//  STORM
+//
+//  Created by 이지윤 on 2020/07/07.
+//  Copyright © 2020 Team STORM. All rights reserved.
+//
+
+import UIKit
+
+class ProjectWaitingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
