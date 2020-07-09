7617a668be89fc5fa19e0e490a5a04fd648975b8
j2reigh <58107794+j2reigh@users.noreply.github.com>
2020-07-09 23:06:25 +0900

    Add assets

---
+++ STORM/STORM/Drawing/DrawingViewController.swift
@@ -0,0 +1,102 @@
+//
+//  DrawingViewController.swift
+//  STORM
+//
+//  Created by seunghwan Lee on 2020/07/03.
+//  Copyright © 2020 Team STORM. All rights reserved.
+//
+
+// feature - Drawing 
+
+import UIKit
+
+class DrawingViewController: UIViewController {
+    
+//    let canvas = Canvas()
+    
+    @IBOutlet weak var projectName: UILabel!
+    @IBOutlet weak var round: UILabel!
+    @IBOutlet weak var remainingTime: UILabel!
+    @IBOutlet weak var canvasView: Canvas!
+    @IBOutlet weak var applicationBtn: UIButton!
+    
+    
+//    let undoButton: UIButton = {
+//        let button = UIButton(type: .system)
+//        button.setTitle("undo", for: .normal)
+//        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+//        button.addTarget(self, action: #selector(handleUndo) , for: .touchUpInside)
+//        return button
+//    }()
+    
+//    @objc fileprivate func handleUndo() {
+//        canvas.undo()
+//    }
+    
+//    let clearButton: UIButton = {
+//        let button = UIButton(type: .system)
+//        button.setTitle("clear", for: .normal)
+//        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+//        button.addTarget(self, action: #selector(handleClear) , for: .touchUpInside)
+//        return button
+//    }()
+    
+//    @objc fileprivate func handleClear() {
+//        canvas.clear()
+//    }
+    
+//    let restoreButton: UIButton = {
+//        let button = UIButton(type: .system)
+//        button.setTitle("restore", for: .normal)
+//        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+//        button.addTarget(self, action: #selector(handleRestore) , for: .touchUpInside)
+//        return button
+//    }()
+    
+//    @objc fileprivate func handleRestore() {
+//        canvas.restore()
+//    }
+
+//    override func loadView() {
+//        self.canvasView = canvas
+//        print("로드뷰 실행")
+//    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        canvasView = canvas
+//        canvasView.layer.cornerRadius = 10
+//        self.canvasView = Canvas()
+        let img = UIImage(named: "red_navigation_bar")
+        navigationController?.navigationBar.setBackgroundImage(img, for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        let titmeImg = UIImage(named: "img_logo")
+        let imageView = UIImageView(image:titmeImg)
+        self.navigationItem.titleView = imageView
+    }
+        
+    @IBAction func didPressText(_ sender: UIButton) {
+    }
+    
+    @IBAction func didPressDrawing(_ sender: UIButton) {
+    }
+    
+    @IBAction func didPressUnDo(_ sender: UIButton) {
+        canvasView.undo()
+    }
+    
+    @IBAction func didPressReDo(_ sender: UIButton) {
+        canvasView.restore()
+    }
+    
+    @IBAction func didPressClean(_ sender: UIButton) {
+        canvasView.clear()
+    }
+    
+    @IBAction func didPressApplication(_ sender: UIButton) {
+    }
+    
+}
+
+
