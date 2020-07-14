e5a684f513da117216c8de5b24cfd18316bce9a6
Lee424 <tmdghks424@naver.com>
2020-07-15 04:58:17 +0900

    [Add] Network Models

---
+++ STORM/STORM/Network/Member.swift
@@ -0,0 +1,12 @@
+//
+//  Member.swift
+//  STORM
+//
+//  Created by seunghwan Lee on 2020/07/14.
+//  Copyright © 2020 Team STORM. All rights reserved.
+//
+
+struct Member: Codable {
+    let user_name: String
+    let user_img: String
+}
