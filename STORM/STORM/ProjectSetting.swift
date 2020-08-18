8a3af715ca7916ae9cb33910f981c27ecc19f5f9
Lee424 <tmdghks424@naver.com>
2020-08-18 17:09:40 +0900

    [Add] ProjectSetting

---
+++ STORM/STORM/ProjectSetting.swift
@@ -0,0 +1,25 @@
+//
+//  ProjectSetting.swift
+//  STORM
+//
+//  Created by seunghwan Lee on 2020/08/08.
+//  Copyright © 2020 Team STORM. All rights reserved.
+//
+
+enum Mode {
+    case host
+    case member
+}
+
+class ProjectSetting {
+    static let shared = ProjectSetting()
+    
+    var mode: Mode = .host
+    var projectIdx: Int?
+    var projectCode: String?
+    var roundNumb: Int?
+    var roundIdx: Int?
+    var roundTime: Double = 0
+    var roundPurpose: String = ""
+    var projectName: String = ""
+}
