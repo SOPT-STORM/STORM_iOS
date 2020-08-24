0108cdc1f2370fe37ad06664ac6c945fbfda3861
Lee424 <tmdghks424@naver.com>
2020-08-25 01:03:55 +0900

    [Add] view transition protocol 추가

---
+++ STORM/STORM/ViewTransitionDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  ViewTransitionDelegate.swift
+//  STORM
+//
+//  Created by seunghwan Lee on 2020/08/18.
+//  Copyright © 2020 Team STORM. All rights reserved.
+//
+
+
+protocol PresentVC {
+    func presentVC()
+}
+
+protocol PushVC {
+    func pushVC()
+}
