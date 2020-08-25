594ab3acb0c48593d3214c588ec77b76da50fdd1
Jihyun247 <wlguslove12345@gmail.com>
2020-08-25 14:00:38 +0900

    [add] go back popup

---
+++ STORM/STORM/PopUpViews/GoBackPopUpViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GoBackPopUpViewController.swift
+//  STORM
+//
+//  Created by 김지현 on 2020/08/19.
+//  Copyright © 2020 Team STORM. All rights reserved.
+//
+
+import UIKit
+
+class GoBackPopUpViewController: UIViewController {
+    
+    @IBOutlet weak var popUpView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        popUpView.roundCorners(corners: .allCorners, radius: 15.0)
+    }
+    
+    @IBAction func okButtonDidTap(_ sender: UIButton) {
+        NotificationCenter.default.post(
+        name: NSNotification.Name(rawValue: "OKButton"),
+        object: nil)
+        self.dismiss(animated: false, completion: nil)
+    }
+        
+    
+    @IBAction func cancelButtonDidTap(_ sender: UIButton) {
+        self.dismiss(animated: false, completion: nil)
+    }
+    
+
+}
